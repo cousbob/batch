5962e5f42b35af77bf2652c60db53a06fec13861
leo <cousbob_114@hotmail.com>
2024-03-09 15:17:55 +0800

    新增時間顯示batch

---
+++ 時間顯示.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+:loop
+@echo off
+for /f "delims=" %%a in ('wmic os get localdatetime ^| find "."') do set datetime=%%a
+set datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
+cls
+echo now time is : %datetime%
+timeout /t 1 > nul
+goto loop
